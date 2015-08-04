aafd11e953937cf56bf1ba7f2dc60867359834d9
Corey Chivers <corey.chivers@uphs.upenn.edu>
2015-08-04 16:42:12 -0400

    Yay sublimetext linter and PEP-8!

--- 7min.py
+++ 7min.py
@@ -3,7 +3,8 @@
 import os
 import time
 
-def countdown(sec,activity,tick=True):
+
+def countdown(sec, activity, tick=True):
 
     for t in reversed(range(sec)):
         print str(t) + " " + activity
@@ -11,11 +12,23 @@ def countdown(sec,activity,tick=True):
             os.system(wave_player + ' histicks.wav 2> /dev/null &')
         time.sleep(1)
 
-def do_workout(intervaltime=30,rest=10):
-    activities = ['Jumping Jacks','Wall Sit','Pushup','Situp','Step Up On Chair','Squat','Triceps Dips','Plank','High-Knees Running','Lunge','Pushup and Rotation','Side-Plank']
+
+def do_workout(intervaltime=30, rest=10):
+    activities = ['Jumping Jacks',
+                  'Wall Sit',
+                  'Pushup',
+                  'Situp',
+                  'Step Up On Chair',
+                  'Squat',
+                  'Triceps Dips',
+                  'Plank',
+                  'High-Knees Running',
+                  'Lunge',
+                  'Pushup and Rotation',
+                  'Side-Plank']
     for act in activities:
-        countdown(rest,"REST -- Next up: " + act,tick=False)
-        countdown(intervaltime,act)
+        countdown(rest, "REST -- Next up: " + act, tick=False)
+        countdown(intervaltime, act)
 
 if os.system('which aplay') == 0:
     wave_player = 'aplay'
@@ -29,8 +42,10 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='The 7 minute Workout')
-    parser.add_argument('-t','--intervaltime',help='Do each interval for this many seconds',default=30)
-    parser.add_argument('-r','--rest',help='rest for this many seconds',default=10)
+    parser.add_argument('-t', '--intervaltime',
+                        help='Do each interval for this many seconds',
+                        default=30)
+    parser.add_argument('-r', '--rest', help='rest for this many seconds', default=10)
 
     args = parser.parse_args()
-    do_workout(int(args.intervaltime),int(args.rest))
+    do_workout(int(args.intervaltime), int(args.rest))
