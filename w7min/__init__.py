92c79138472114dcf06fb80db8f1d2cfa571d1bb
Corey Chivers <corey.chivers@uphs.upenn.edu>
2015-10-02 16:58:09 -0400

    data path finder bugfix

--- w7min/__init__.py
+++ w7min/__init__.py
@@ -1,9 +1,13 @@
+"""w7min: The scientific 7minute workout
+
+.. moduleauthor:: Corey Chivers <corey.chivers@mail.mcgill.ca>
+
+"""
 from os.path import dirname, join, exists
 
-import w7min
 
 def _get_file(c_file):
-    def_path = join(dirname(w7min.__file__), c_file)
+    def_path = join(dirname(__file__), c_file)
     if exists(def_path):
         return def_path
     else:
